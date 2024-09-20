63cce3642227cb2a84a548cd78217a0cde6602d6
Josue-Adiel <extremeboys14@gmail.com>
2024-09-20 16:19:05 -0400

    Combined the SendData example with our code to measure the distance with the sensor in the arduino

--- ArduinoSendData/ArduinoSendData.ino
+++ ArduinoSendData/ArduinoSendData.ino
@@ -1,11 +1,22 @@
 
 //      ******************************************************************
 //      *                                                                *
-//      *                                                                *
 //      *     Example Arduino program that transmits data to a laptop    *
 //      *                                                                *
-//      *                                                                *
 //      ******************************************************************
+
+#include <Servo.h>
+int servoX = 13;   // Declare the X servo pin
+int servoY = 12; // Declare the Y servo pin
+Servo ServoX;        // Create a servo object 
+Servo ServoY;
+int sensorPin=A0;    // Declare the pins for the sensor input and output
+float distance;       // The distance measurements from the sensor
+float sensorInput;       // The time it takes for the sensor to recieve the signal it sends
+int(offTime);
+int degX;
+int degY;
+int degChange = 20;
 
 
 //
@@ -18,6 +29,10 @@ void setup()
   //
   long baudRate = 9600;       // NOTE1: The baudRate for sending & receiving programs must match
   Serial.begin(baudRate);     // NOTE2: Set the baudRate to 115200 for faster communication
+
+  ServoX.attach(servoX);
+  ServoY.attach(servoY);
+  Serial.begin(9600);
 }
 
 
@@ -25,44 +40,60 @@ void setup()
 //
 // main loop
 //
-void loop() 
-{  
-  int a, b, c, d;
 
-  a = 0;
-  b = 0;
-  c = 0;
-  d = 0;
-
-  //
-  // loop: calculate the data, then send it from the Arduino to the phython program
-  //
-  while(true) {
-    //
-    // here is where you update the data to be sent
-    //
-    a = a + 1;    // a counts by 1s
-    b = b + 2;    // b counts by 2s
-    c = c + 3;    // c counts by 3s
-    d = d + 4;    // d counts by 4s
+void loop()
+{
+  
+  for (degX = 0; degX<= 180; degX = degX + degChange)
+  {
+  ServoX.write(degX); // Make servo go to 0 degrees 
+  ServoY.write(0);
+  delay(100);
+  	for (degY = 0; degY<= 180; degY = degY + degChange)
+    {
+      ServoY.write(degY); // Make servo go to 0 degrees 
+      read();
+      delay(100);
+    }
     
-    
-    //
-    // transmit one line of text to phython with 4 numeric values
-    // NOTE: commas are sent between values, after the last value a Newline is sent
-    //
-    Serial.print(a);    Serial.print(",");
-    Serial.print(b);    Serial.print(",");
-    Serial.print(c);    Serial.print(",");
-    Serial.println(d);
-    
-
-    //
-    // delay after sending data so the serial connection is not over run
-    //
-    delay(400);
+  
+  delay(100);
   }
+   delay(offTime);
 }
 
 
 
+void read()
+{
+  sensorInput=analogRead(sensorPin);
+  distance=sensorInput;
+  // debug();
+  csv();
+  delay(100);
+}
+void csv()
+{
+  // Code to print sensor values as they would be seen in a csv
+  Serial.print("\n");
+  Serial.print(distance);
+  Serial.print(",");
+  Serial.print(degX);
+  Serial.print(",");
+  Serial.print(degY);
+}
+void debug()
+{
+  // Code to print sensor values with units
+  Serial.print("\n Distance: ");
+  Serial.print(distance);
+  Serial.print(" cm");
+  
+  Serial.print(" | X: ");
+  Serial.print(degX);
+  Serial.print(" degrees");
+  
+  Serial.print(" | Y: ");
+  Serial.print(degY);
+  Serial.print(" degrees");
+}
